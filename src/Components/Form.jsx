e45981381bc799d6637428cf6be94e768e9496dc
ana <ana@anniecannons.com>
2023-12-21 13:27:12 -0800

    trying to fix the error

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,36 +3,43 @@
 import "../App.css";
 
 function Form({
-    onClickProp,
+  onClickProp,
   bookDataProp,
   selectedCoverTypeProp,
   selectedCategoryProp,
   handleCategoryChangeProp,
   handleCoverTypeChangeProp,
+  errorMessageProp,
 }) {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onClickProp(selectedCoverTypeProp, selectedCategoryProp); // Pass selected values to the parent component
-      };
-      
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onClickProp(selectedCoverTypeProp, selectedCategoryProp);
+  };
+
+  console.log(errorMessageProp, "error");
+  console.log(bookDataProp, "bookDataProp,");
   return (
     <>
-    
       <div className="form-container">
         <h1>Check out the New York Times Best Sellers Lists for Books!</h1>
       </div>
 
       <form onSubmit={handleSubmit}>
-        <label>Paperback or Hardcover?</label>
-
-        <select  value={selectedCoverTypeProp} onChange={handleCoverTypeChangeProp}>
+        <select
+          value={selectedCoverTypeProp}
+          onChange={handleCoverTypeChangeProp}
+        >
           <option value="select">Select A Cover Type</option>
           <option value="paperback">Paperback</option>
           <option value="hardcover">Hardcover</option>
         </select>
 
         <label>Fiction or NonFiction?</label>
-        <select   value={selectedCategoryProp} onChange={handleCategoryChangeProp}>
+        <select
+          value={selectedCategoryProp}
+          onChange={handleCategoryChangeProp}
+        >
           <option value="select">Select A Category</option>
           <option value="fiction">Fiction</option>
           <option value="nonfiction">NonFiction</option>
@@ -42,16 +49,6 @@ function Form({
           <button type="submit">Click</button>
         </div>
       </form>
-      <div className="mapbook-parent-container">
-      {bookDataProp &&
-        bookDataProp.results.books.map((item, key) => (
-          <div className="books-container" key={key}>
-            <p> {key + 1}</p>
-            <p>{item.title}</p>
-            <img src={item.book_image} alt={`Book cover for ${item.title}`} />
-          </div>
-        ))}
-           </div>
     </>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,6 +6,10 @@ function Home() {
   const [bookData, setBookData] = useState(null);
   const [selectedCoverType, setSelectedCoverType] = useState("select");
   const [selectedCategory, setSelectedCategory] = useState("select");
+  const [errorMessage, setErrorMessage]=useState(null)
+
+  
+
 
   const fetchData = async (coverType, category) => {
     const apiUrl = `https://api.nytimes.com/svc/books/v3/lists/current/${coverType}-${category}.json?api-key=uKtQimMDWaJKpvlcydyoRgLnrgAZju5k`;
@@ -17,6 +21,7 @@ function Home() {
       setBookData(data);
     } catch (error) {
       console.error("Error fetching data:", error);
+      setErrorMessage(true);
     }
   };
 
@@ -39,9 +44,20 @@ function Home() {
   selectedCategoryProp={selectedCategory}
   handleCoverTypeChangeProp={handleCoverTypeChange}
   handleCategoryChangeProp={handleCategoryChange}
+  errorMessageProp={ errorMessage}
 />
+<div className="mapbook-parent-container">
+  {bookData &&
+    bookData.results.books.map((item, key) => (
+      <div className="books-container" key={key}>
+        
+        <p>{key + 1}</p>
+        <p>{item.title}</p>
+        <img src={item.book_image} alt={`Book cover for ${item.title}`} />
+      </div>
+    ))}
+</div>
 
-      
     </>
   );
 }
