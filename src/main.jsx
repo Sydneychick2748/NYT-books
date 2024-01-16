d5f664e4ae14017ae28b7004c10fd9595f018ba6
ana <ana@anniecannons.com>
2024-01-16 10:11:36 -0800

    addeed error

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary fallback =" this is an invalid combination. please refresh the page and try again "/>
+    <ErrorBoundary fallback =" this is an invalid combination. please refresh the page and try again ">
     <App />
+    </ErrorBoundary>
+
+    
   </React.StrictMode>,
 )
