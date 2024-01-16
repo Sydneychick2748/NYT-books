caa2d9bd3e273781c7ad72dc22cb3abc4c0c72ae
ana <ana@anniecannons.com>
2024-01-16 09:55:05 -0800

    added the error message

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ErrorBoundary fallback =" this is an invalid combination. please refresh the page and try again "/>
     <App />
   </React.StrictMode>,
 )
